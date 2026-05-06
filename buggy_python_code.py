a5a1b54121366b2b3ca26f20c1e9f55efeb36e74
xcimra <xcimra@github.com>
2026-05-06 12:56:22 +0200

    fix codeql

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -25,7 +25,8 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    if urllib_version == "2" or "3":
+        exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
     try: 
